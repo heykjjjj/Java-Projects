8ab8632e09d28f4a262d707a0130d137ab5d8365
Khervin John Quimora <109788411+heykjjjj@users.noreply.github.com>
2024-02-18 16:23:34 +0800

    Delete SimpleChattyBot/SimpleChattyBot directory

--- SimpleChattyBot/SimpleChattyBot/src/stages/Stage1.java
+++
@@ -1,8 +0,0 @@
-package stages;
-
-public class Stage1 {
-    public static void main(String[] args) {
-        System.out.println("Hello! My name is Kj Bot.");
-        System.out.println("I was created in 2023.");
-    }
-}
